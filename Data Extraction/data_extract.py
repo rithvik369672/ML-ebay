9d8fa70eb4fadab06df3a1b2540fa9527f8dd615
Ray08charles <45870004+red574890@users.noreply.github.com>
2020-11-30 23:56:11 +0800

    Update data_extract.py

--- Data Extraction/data_extract.py
+++ Data Extraction/data_extract.py
@@ -7,10 +7,11 @@ data.columns=["Category","primary_image_url","all_image_url","attribute","index"
 
 
 
-n=1000 #enter the number of data that want to extract
+n=10 #enter the number of data that want to extract
 
 #add empty column
 data["Brand"]=""
+data["Color"]=""
 data["US Shoe Size"]=""
 data["Style"]=""
 data["Material"]=""
@@ -18,23 +19,36 @@ data["Country/Region of Manufacture"]=""
 data["Width"]=""
 
 def entrydata(data,column,i):    #column is the colname datatype:string;
-    try:
-        a=re.search(column,data["attribute"][i]).span()
-        temp=data["attribute"][i][a[1]+1:]
-        a=re.search(',',temp).span()
-        data[column][i]=temp[0:a[0]]
-    except AttributeError:
+    
+    a=re.search(column,data["attribute"][i])
+    if(a==None):
         data[column][i]="N\A"
+    else:
+        loc=a.span()
+        temp=data["attribute"][i][loc[1]+1:]
+        a=re.search(',',temp)
+        if(a==None):
+            data[column][i]=temp
+        else:
+            loc=a.span()
+            data[column][i]=temp[0:loc[0]]
+
 
 
 def entrydatashoesize(data,column,i):    #column is the colname datatype:string;
-    try:
-        a=re.search(column,data["attribute"][i]).span()
-        temp=data["attribute"][i][a[1]+9:]
-        a=re.search(',',temp).span()
-        data[column][i]=temp[0:a[0]]
-    except AttributeError:
+    a=re.search(column,data["attribute"][i])
+    if(a==None):
         data[column][i]="N\A"
+    else:
+        loc=a.span()
+        temp=data["attribute"][i][loc[1]+9:]
+        a=re.search(',',temp)
+        if(a==None):
+            data[column][i]=temp
+        else:
+            a=re.search(',',temp).span()
+            data[column][i]=temp[0:a[0]]
+
 
     
 
@@ -44,6 +58,7 @@ for i in range(0,n):
     data["attribute"][i] =data["attribute"][i][0:len( data["attribute"][i])-1]
 
     entrydata(data,"Brand",i)
+    entrydata(data,"Color",i)
     entrydatashoesize(data,"US Shoe Size",i)
     entrydata(data,"Style",i)
     entrydata(data,"Material",i)
